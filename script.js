cabf5bad340f349265bf82cb8b9b9b09800afa4b
Thomas Roth <thomas.roth@aon.at>
2024-03-30 16:44:44 +0100

    delete non-functional code
    and also changed loop to assign unique id's
    instead of a class to the divs

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const divContainer = document.querySelector("#container");
 
 for (let i = 0; i < (16*16); i++) {
     let gridDiv = document.createElement("div");
-    gridDiv.setAttribute("class", "gridDiv");
+    gridDiv.setAttribute("id", `gridDiv${i}`);
     divContainer.appendChild(gridDiv);
 }
 
-let drawDiv = document.querySelector(".gridDiv");
-drawDiv.addEventListener("mouseover", () => {
-    div.setAttribute("style", "background: black");
-});
+
