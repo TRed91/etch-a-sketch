00cf2520590dec0557c031e782b150756781c07a
Thomas Roth <thomas.roth@aon.at>
2024-04-01 14:35:24 +0200

    fix div container becoming black on hover
    used a loop to target each generated square
    individually with the even listener.
    also used getElementByClassName because
    querySelector doesn't assign an index to each
    element with that class.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btn = document.querySelector("#btn");
 let numberOfSquares = 16;
 
 btn.addEventListener("click", () => {
-    
+
     divContainer.style.background = "white";
     while (divContainer.firstChild) {
         divContainer.firstChild.remove();
@@ -12,23 +12,26 @@ btn.addEventListener("click", () => {
     let input = Number(prompt("Select number of squares per side", ""));
     
     if (input >= 16 && input <= 100) {
+        
        numberOfSquares = input;
        drawGrid();
        } else {
         alert("Choose a number between 16 and 100!");
     }
 
-    divContainer.addEventListener("mouseover", (event) => {
-        let target = event.target;
-        target.style.backgroundColor = "black";
-    });
+    const square = document.getElementsByClassName("square");
+    for (let j = 0; j < square.length; j++)
+        square[j].addEventListener("mouseover", (event) => {
+            let target = event.target;
+            target.style.backgroundColor = "black";
+        });
 });
 
 // draw grid
 function drawGrid (){
 for (let i = 0; i < (numberOfSquares ** 2); i++) {
     let gridDiv = document.createElement("div");
-    gridDiv.setAttribute("id", `gridDiv${i}`);
+    gridDiv.setAttribute("class", "square");
     gridDiv.style.width = Math.ceil(960/numberOfSquares)+"px";
     gridDiv.style.height = Math.ceil(960/numberOfSquares)+"px";
     divContainer.appendChild(gridDiv);
